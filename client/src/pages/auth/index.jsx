3b7b1e1582742fb0bedfde89c7d8caadd0e3ccef
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-20 20:03:36 +0300

    auth

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Background from '../../assets/login2.png';
 import Victory from '@/assets/victory.svg'
+import { Tabs, TabsList, TabsContent, TabsTrigger } from '@/components/ui/tabs';
+import { Input } from '@/components/ui/input'
+import {Button} from '@/components/ui/button'
 
 const Auth = () => {
+    const [email, setemail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setconfirmPassword] = useState("")
+
+    const handleLogin = async ()=>{
+
+    }
+
+    const handleSignup = async()=>{
+
+    }
+
     return (
         <div className="h-[100vh] w-[100vw] flex items-center justify-center">
             <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
@@ -15,7 +30,38 @@ const Auth = () => {
                         <p className="font-medium text-center">Fill in the details to get started with the best chat app!</p>
                     </div>
                     <div className="flex items-center justify-center w-full">
-                        
+                        <Tabs className="w-3/4">
+                            <TabsList className="bg-transparent rounded-none w-full">
+                                <TabsTrigger value="login" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all">Login</TabsTrigger>
+                                <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all">SignUp</TabsTrigger>
+                            </TabsList>
+                            <TabsContent className="flex flex-col gap-5 mt-10" value="login">
+                                <Input placeholder="Email" type="email"
+                                    className="rounded-full p-6"
+                                    value={email}
+                                    onChange={(e) => setemail(e.target.value)} />
+                                <Input placeholder="Password" type="password"
+                                    className="rounded-full p-6"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)} />
+                                <Button className="rounded-full p-6" onClick={handleLogin}>Login</Button>
+                            </TabsContent>
+                            <TabsContent className="flex flex-col gap-5" value="signup">
+                                <Input placeholder="Email" type="email"
+                                        className="rounded-full p-6"
+                                        value={email}
+                                        onChange={(e) => setemail(e.target.value)} />
+                                    <Input placeholder="Password" type="password"
+                                        className="rounded-full p-6"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)} />
+                                     <Input placeholder="Confirm Password" type="password"
+                                        className="rounded-full p-6"
+                                        value={confirmPassword}
+                                        onChange={(e) => setconfirmPassword(e.target.value)} />
+                                        <Button className="rounded-full p-6" onClick={handleSignup}>SignUp</Button>
+                            </TabsContent>
+                        </Tabs>
                     </div>
                 </div>
             </div>
