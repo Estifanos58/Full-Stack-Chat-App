2bf8b3d018ef4ee1e2c98032ee7814539580231a
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-23 13:32:12 -0800

    contact and empity message complonen

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,6 +1,9 @@
 import { useAppStore } from '@/store'
 import React,{useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
+import ContactContainer from './component/contact-container';
+import EmptyChatContainer from './component/empty-chat-container';
+import ChatContainer from './component/chat-container';
 
 const Chat = () => {
   const { userInfo } = useAppStore();
@@ -11,7 +14,11 @@ const Chat = () => {
     }
   },[userInfo, navigate])
   return (
-    <div>Chat</div>
+    <div className='flex h-[100vh] text-white overflow-hidden'>
+      <ContactContainer/>
+      <EmptyChatContainer/>
+      <ChatContainer/>
+    </div>
   )
 }
 
