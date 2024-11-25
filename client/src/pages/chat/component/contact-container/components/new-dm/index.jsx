99e4b9f3dd1d6e2b583f0d6634a51296a93bcad5
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-25 18:28:40 +0300

    search feature

--- client/src/pages/chat/component/contact-container/components/new-dm/index.jsx
+++ client/src/pages/chat/component/contact-container/components/new-dm/index.jsx
@@ -1,65 +1,126 @@
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-  } from "@/components/ui/tooltip"
-import { FaPlus } from "react-icons/fa"
-import { useState } from "react"
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { FaPlus } from "react-icons/fa";
+import { useState } from "react";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-  
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { apiClient } from "@/lib/api-client";
+import { SEARCH_CONTACTS_ROUTES } from "@/utils/constants";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Avatar } from "@/components/ui/avatar";
+import { AvatarImage } from "@/components/ui/avatar";
+import { getColor } from "@/lib/utils";
 
 const NewDm = () => {
-    const [openNewContact, setOpenNewContact] = useState(false)
+  const [openNewContact, setOpenNewContact] = useState(false);
 
-    const searchContacts = async (searchTerm)=>{}
-    const [searchedContacts, setSetsearchedContacts] = useState([]);
+  const searchContacts = async (searchTerm) => {
+    try {
+      if (searchTerm.length > 0) {
+        const response = await apiClient.post(
+          SEARCH_CONTACTS_ROUTES,
+          { searchTerm },
+          { withCredentials: true }
+        );
+        console.log(response.data.contacts)
+        if (response.status === 200 && response.data.contacts) {
+          setSetsearchedContacts(response.data.contacts);
+        }
+      } else {
+        setSetsearchedContacts([]);
+      }
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+  const [searchedContacts, setSetsearchedContacts] = useState([]);
+  console.log(searchedContacts)
 
   return (
     <>
-        <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger>
-                <FaPlus className="text-neutral-400 font-light text-opacity-90 text-start hover:text-neutral-100 cursor-pointer transition-all duration-300 "  onClick={()=> setOpenNewContact(true)}/>
-              </TooltipTrigger>
-              <TooltipContent className="bg-[#1c1b1e] border-none mb-2 p-2 text-white">
-                Select New Contact
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-          <Dialog open={openNewContact} onOpenChange={setOpenNewContact}>
-                <DialogContent className="bg-[#181920] border-none text-white w-[480px] h-[400px] flex flex-col">
-                    <DialogHeader>
-                    <DialogTitle>Please select a contact</DialogTitle>
-                    <DialogDescription>
-                        
-                    </DialogDescription>
-                    </DialogHeader>
-                    <div>
-                        <Input
-                        placeholder="Search Contacts"
-                        className="rounded-lg p-6 bg-[#2c2e3b] border-none "
-                        onChange={(e)=>searchContacts(e.target.value)}
-                       />
-                    </div>
-                    {
-                        searchedContacts.length <=0 && (<div className="text-2xl flex flex-col justify-center items-center h-[100%]">
-                            Their is no Contact😒
-                        </div>)
-                    }
-                </DialogContent>
-            </Dialog>
-
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <FaPlus
+              className="text-neutral-400 font-light text-opacity-90 text-start hover:text-neutral-100 cursor-pointer transition-all duration-300 "
+              onClick={() => setOpenNewContact(true)}
+            />
+          </TooltipTrigger>
+          <TooltipContent className="bg-[#1c1b1e] border-none mb-2 p-2 text-white">
+            Select New Contact
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <Dialog open={openNewContact} onOpenChange={setOpenNewContact}>
+        <DialogContent className="bg-[#181920] border-none text-white w-[480px] h-[400px] flex flex-col">
+          <DialogHeader>
+            <DialogTitle>Please select a contact</DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div>
+            <Input
+              placeholder="Search Contacts"
+              className="rounded-lg p-6 bg-[#2c2e3b] border-none "
+              onChange={(e) => searchContacts(e.target.value)}
+            />
+          </div>
+          <ScrollArea className="h-[250px]">
+            <div className="flex flex-col gep-5">
+              {searchedContacts?.map((contact) => (
+                <div className="flex gap-3 items-center cursor-pointer">
+                  <div className="w-12 h-12 relative">
+                    <Avatar className="h-12 w-12  rounded-full overflow-hidden">
+                      {contact?.image ? (
+                        <AvatarImage
+                          src={`${HOST}${contact.image}`}
+                          alt="profile"
+                          className="object-cover w-full h-full bg-black"
+                        />
+                      ) : (
+                        <div
+                          className={`uppercase h-12 w-12 text-lg border-[1px] flex items-center justify-center rounded-full ${getColor(
+                            contact?.color
+                          )}`}
+                        >
+                          {contact?.firstname
+                            ? contact?.firstname.split("").shift()
+                            : contact.email.split("").shift()}
+                        </div>
+                      )}
+                    </Avatar>
+                  </div>
+                  <div className="flex flex-col">
+                    <span>
+                       {contact?.firstname && contact?.lastname
+                      ? `${contact.firstname} ${contact.lastname}`
+                      : contact.email}
+                    </span>
+                    <span className="text-xs">{contact.email}</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </ScrollArea>
+          {searchedContacts.length <= 0 && (
+            <div className="text-2xl flex flex-col justify-center items-center h-[100%]">
+              Their is no Contact😒
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
     </>
-  )
-}
+  );
+};
 
-export default NewDm
+export default NewDm;
