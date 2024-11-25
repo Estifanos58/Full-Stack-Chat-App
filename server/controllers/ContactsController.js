32b60e8882f28cebf5edba842122b98fdab41186
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-25 18:31:40 +0300

    search controller

--- server/controllers/ContactsController.js
+++ server/controllers/ContactsController.js
@@ -1,9 +1,9 @@
-import User from "../models/UserModel";
+import User from "../models/UserModel.js";
 
 export const searchContacts = async (req, res) => {
     try {
       const {searchTerm} = req.body;
-
+      console.log(searchTerm)
       if(searchTerm === undefined || searchTerm === null){
         return res.status(400).send("Searchterm is required");
       }
@@ -11,6 +11,10 @@ export const searchContacts = async (req, res) => {
       const sanitizedSearchTerm = searchTerm.replace(/[.*+?^${}{}|[\]\\]/g, "\\$&");
 
       const regex = new RegExp(sanitizedSearchTerm, "i");
+
+      if (!req.userId) {
+        return res.status(400).send("User ID is required");
+      }
 
       const contacts = await User.find({
         $and: [
